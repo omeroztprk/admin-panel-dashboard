29413fcc4c0d14e01c181def8cd6d23a4e563b68
omeroztprk <omeroztoprak003@gmail.com>
2025-09-12 03:14:29 +0300

    chore: remove tracked environment.ts from repo

--- frontend/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-const hostname = window.location.hostname;
-const apiHost = hostname === 'localhost'
-  ? 'http://localhost:3000'
-  : 'http://192.168.1.158:3000';
-
-export const environment = {
-  production: false,
-  apiUrl: `${apiHost}/api`
-};
